f80a8c973f8b08b04a0d2964a133b720cb826047
Alexandre Switalski <alexandre.swit@gmail.com>
2022-05-02 10:55:41 +0200

    Initial commit

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
+
+<script lang="ts" setup>
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <p>it works</p>
+  </div>
+</template>
