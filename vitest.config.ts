12d0cfece4217f71ced03b4c62a81ae9afaf53b2
Julien Martin <julienmartin@codeassist.io>
2022-05-09 16:54:54 +0200

    fix(*): vitest autoimport (#2)

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
 
 export default defineConfig({
   plugins: [
     Vue(),
+    AutoImport({
+      imports: [
+        'vue',
+      ],
+    }),
   ],
   test: {
     globals: true,
